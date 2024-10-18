bd1a16fbcfac2a7ff3adf55ce6462c053a15c971
omanthi <omanthibandara326@gmail.com>
2024-10-18 15:13:33 +0530

    Phython animal file

---
+++ animal.py
@@ -0,0 +1,74 @@
+import sys
+
+def default():
+    print("hellow")
+
+def main():
+        default()
+if __name__=='__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
